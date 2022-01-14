f5f8ace4e68bc8d492d2e25e5d5bbb87ecdc0e0c
Ella Ahlstedt <ella.ahlstedt@learnet.se>
2022-01-14 09:19:20 +0100

    structure

--- dispatcher.js
+++ dispatcher.js
@@ -5,64 +5,61 @@ const cpu1Input = document.getElementById('cpu-1-input');
 const cpu2Input = document.getElementById('cpu-2-input');
 const cpu3Input = document.getElementById('cpu-3-input');
 
-// Klassdeklaration för noden som innehåller data och pekaren till nästa nod
 
-class Node {
-    constructor(data, next=null) {
-        this.data = data;
-        console.log("Data: " + data);
-        this.next = next;
+
+class Process {
+    constructor(name, execTime, priority) {
+        this.name = name;
+        this.execTime = execTime;
+        this.priority = priority;
+        this.remainingTime = this.execTime;
     }
 }
 
-// Huvudet för listan som innehåller enbart EN pekare till första noden i listan
-
-class LinkedList {
+class CPU1 {
     constructor() {
-        this.head = null;
+        this.queueLength = 0;
+        this.queue = null;
     }
-    insert(data) {
-        let t = new Node(data);
-        t.next = this.head;
-        this.head = t;
-        console.log('Added new node');
+    add(process) {
+        // längst bak
     }
-    showAll() {
-        let t = this.head;
-        while(t != null) {
-            t = t.next;
-        }
+    remove(process) {
+        // första
+    } 
+    work(ms) {
+        // metod1
     }
-    generate(num) {
-        for(let i = 0; i < num; i++) {
-            this.insert(Math.floor(Math.random()));
-        }
-    }
-    deleteTail() {
-        if (this.head == null || this.head.next == null) 
-        return;
-        let t = this.head;
-        while(t.next.next != null) {
-            t = t.next;
-        }
-        t.next = null;
-    }
-    deleteHead() {
-        if (this.head != null) {
-            this.head = this.head.next;
-        }
-    }
-    length() {
-        let t = this.head;
-        let num = 0;
-        while( t != null ) 
-        {
-            num++;
-            t=t.next;
-        }
-        return num;
-    }
-    sort() {
+}
 
+class CPU2 {
+    constructor() {
+        this.queueLength = 0;
+        this.queue = null;
+    }
+    add(process) {
+        // längst bak
+    }
+    remove(process) {
+        // specifierad
+    } 
+    work(ms) {
+        // metod2
+    }
+}
+
+class CPU3 {
+    constructor() {
+        this.queueLength = 0;
+        this.queue = null;
+    }
+    add(process) {
+        // i prioritet
+    }
+    remove(process) {
+        // första
+    } 
+    work(ms) {
+        // metod3
     }
 }
